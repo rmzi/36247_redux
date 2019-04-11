92ac9f6ae4f87add86eb04ed4bb9150bb776ab06
Ana <agiraldow@gmail.com>
2019-04-11 15:00:37 -0400

    Add and center arrows

--- site/src/App.js
+++ site/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import background from './img/bg3.png'
+import dopeArrow from './img/36-like-sm-bw-dn2.png'
+import wotArrow from './img/36-wot-sm-bw-cn.png'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <img src={background}></img>
+      <div class="arrows">
+        <img src={dopeArrow}></img>
+        <img src={wotArrow}></img>
       </div>
     )
   }
