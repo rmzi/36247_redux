860b54acfcbb7bfd522b88728f201ab65a866d09
Ana <agiraldow@gmail.com>
2019-04-11 14:43:47 -0400

    Add images, update background image

--- site/src/App.js
+++ site/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import background from './img/bg3.png'
 
 class App extends Component {
   render() {
     return (
-      <div> hello</div>
-    );
+      <div>
+        <img src={background}></img>
+      </div>
+    )
   }
 }
 
